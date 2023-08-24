f3abf524a5926971a1285395d19c03df79b5b3d7
devsylva <sylvaejike@gmail.com>
2023-08-24 12:06:07 +0100

    wrote some basic tests

--- src/task_app/tests.py
+++ src/task_app/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Task, Category, Tag
+
 
 # Create your tests here.
+User = get_user_model()
+
+class TaskModelTest(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email='testemail@gmail.com',
+            password="Password123"
+        )
+        self.category = Category.objects.create(
+            name="Study",
+            user=self.user
+        )
+        self.tag = Tag.objects.create(
+            name="tag 1",
+            user=self.user
+        )
+
+    def test_create_task(self):
+        task = Task.objects.create(
+            user=self.user,
+            title="Task title",
+            description="This is a test task",
+            category=self.category,
+            tag=self.tag
+        )
+
+        self.assertEqual(task.title, "Task title")
+        self.assertEqual(task.user, self.user)
--- src/user_app/tests.py
+++ src/user_app/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model 
+
 
 # Create your tests here.
+User = get_user_model()
+
+class UserModelTest(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create_user(
+            email="testemail@gmail.com",
+            password="password123"
+        )
+        self.assertEqual(user.email, "testemail@gmail.com")
+        self.assertFalse(user.is_verified)
+        self.assertTrue(user.check_password("password123"))
+
+
+    def test_create_superuser(self):
+        superuser = User.objects.create_superuser(
+            email="adminemail@gmail.com",
+            password="adminpassword@gmail.com"
+        )
+
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_superuser)
+        self.assertTrue(superuser.is_verified)
